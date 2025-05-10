ea77d815bd5b14263edc354a99ad4e2fb839b783
tahmeedhossain455 <tahmeed.hossain@murdoch.edu.au>
2025-05-11 00:44:23 +0800

    RDSML day3 coding

---
+++ RDSML-Day-3 👉 R Programming Basics (Part-1)/r_basics part1.R
@@ -0,0 +1,10 @@
+x<-11
+x+10
+my_age<- 30
+my_weight<-70
+my_height<-68
+x<- (my_height*my_age)/my_weight
+print (x)
+#to keep multiple number in a single variable we will use c functions
+students_height <- c(40,34,77,56,44,98)
+mean(students_height)
